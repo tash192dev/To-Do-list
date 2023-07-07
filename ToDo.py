99e50df2d1d0b123bea072a9e2cf2bd6dbfe761e
Talha Ashraf <tash192@aucklanduni.ac.nz>
2023-07-07 14:18:12 +1200

    Added the delete button

--- ToDo.py
+++ ToDo.py
@@ -6,17 +6,11 @@ import tkinter
 from tkinter import *
 from tkinter import messagebox
 
-#--------------ToDo-------------------#
-#Add scroll bar to listbox - decided not to add because just looks celeaner without it
-#Sort out frames and other geometry stuff  
-#Colours/fonts all that jazz    
-#Compile to .exe ?
-#link to external database ?????????????
 #-------------Boilerplate starter stuff----------------------#
 tk = tkinter
 main = Tk()  # Name of main window
 main.title("To Do")  # Title
-main.geometry("250x355")  # Size of the window in pixels
+main.geometry("250x420")  # Size of the window in pixels
 main.configure(bg="pink")  # Background colour
 main.resizable(True, True) #Window can be resized
 
@@ -24,19 +18,22 @@ main.resizable(True, True) #Window can be resized
 entry_frame = LabelFrame(main, text = "Enter Task")
 entry_frame.grid(row = 0, column= 0 )
 
-button_frame = LabelFrame(main)
-button_frame.grid(row= 1, column= 0)
+add_button_frame = LabelFrame(main)
+add_button_frame.grid(row= 1, column= 0)
 
 
 checklist_frame = LabelFrame(main, text = "List")
 checklist_frame.grid(row = 2, column= 0)
 
 
-delete_frame = LabelFrame(main)
-delete_frame.grid(row=3, column=0)
+done_button_frame = LabelFrame(main)
+done_button_frame.grid(row=3, column=0)
 
-completed_frame = LabelFrame(main)
-completed_frame.grid(row = 4, column = 0)
+completed_tasks_counter_frame = LabelFrame(main)
+completed_tasks_counter_frame.grid(row = 4, column = 0)
+
+undo_frame = LabelFrame(main)
+undo_frame.grid(row = 5, column = 0)
 #------------------Entry stuff----------------#
 widget_width = 27
 button_width = 26
@@ -63,14 +60,18 @@ list_box.grid(row = 1, column = 1)
 
 #-----------------------Logic--------------#
 
-#this variable will be used by functions to track ammount of completed tasks
-completed_tasks = len(listbox_list)
+#Initialising to 0 every time the program starts
+completed_tasks_counter = 0
 
-#This helper function will update how many tasks have been completed
-def completed_updater(tasks):
-    tasks_completed = tk.Label(completed_frame, text = 'Completed Tasks : ' + str(completed_tasks), width = button_width - 5, font = (10))
-    tasks_completed.grid(row = 0, column = 2, columnspan = 2)
-completed_updater(completed_tasks)
+#This function changes the number of the variable that counts completed tasks
+def tasks_counter_updater():
+    tasks_completed = tk.Label(completed_tasks_counter_frame, text = 'Completed Tasks : ' + str(completed_tasks_counter), width = button_width, font = (10))
+    tasks_completed.grid(row = 0, column = 0, columnspan = 2)
+
+#this function is called when the program starts to set the counter to the correct value
+tasks_counter_updater()
+
+
 #this function command for the Add button. It adds the text from the text entry box as an element in the listbox
 #checks if the text is empty and if the text is in the listbox_list if it is then it pops up a message box with an error 
 
@@ -99,17 +100,22 @@ def add_button_function():
     
 
 deleted_tasks = []
+last_action_was_done = False
 # this function is the command for the delete button
 # it uses the indicies of all selected items in the listbox with the curselection method. 
 def delete_item():
-    global completed_tasks
+    global completed_tasks_counter
     global listbox_list
     global deleted_tasks
-    deleted_tasks = []
-    # print(listbox_list)
-    #contains a reversed list of the currently selected indecies 
+    global last_action_was_done
     selected_indecies = list(reversed(list_box.curselection()))
-    # print(selected_indecies)
+    if(len(selected_indecies) == 0):    
+        return
+    last_action_was_done = False
+    
+    deleted_tasks = []
+    #contains a reversed list of the currently selected indecies 
+    
     for i in range(len(selected_indecies)):
         list_index = selected_indecies[i]
         #removing element from the listbox
@@ -119,13 +125,21 @@ def delete_item():
         deleted_tasks.append(listbox_list.pop(list_index))
     #putting how many tasks have been "completed" or deleted using this function
     
-    completed_tasks += len(selected_indecies)
-    completed_updater(completed_tasks)
+    # completed_tasks_counter += len(selected_indecies)
 
     #overriting the file with the new listbox_list
     with open("tasks.txt", "w") as file:
         for i in range(len(listbox_list)):
             file.write(listbox_list[i] + "\n")
+    
+    return len(selected_indecies)
+
+def done_function():
+    global last_action_was_done
+    global completed_tasks_counter
+    completed_tasks_counter += delete_item()
+    last_action_was_done = True
+    tasks_counter_updater()
 
 
 #this function reverses the last delete operation
@@ -133,47 +147,30 @@ def delete_item():
 #if mutiple tasks were deleted then it will add all of them back
 def undo():
     global deleted_tasks
-    global completed_tasks
+    global completed_tasks_counter
     if(len(deleted_tasks) != 0):
-        completed_tasks -= len(deleted_tasks)
-        completed_updater(completed_tasks)
+        if(last_action_was_done):
+            completed_tasks_counter -= len(deleted_tasks)
+        tasks_counter_updater()
         for i in range(len(deleted_tasks)):
             add_task(deleted_tasks[i])
         deleted_tasks = []
     with open("tasks.txt", "w") as file:
         for i in range(len(listbox_list)):
             file.write(listbox_list[i] + "\n")
-
-
-#if the file does not exist then create a file called tasks.txt and write the listbox_list to it
-# #if it already exists then read it in and put it in the listbox_list
-# try:
-#     with open("tasks.txt", "r") as file:
-#         listbox_list = file.read().splitlines()
-
-#         print(listbox_list)
-#         for i in range(len(listbox_list)):
-#             add_task(listbox_list[i])
-#         print(listbox_list)
-# except:
-#     with open("tasks.txt", "w") as file:
-#         for i in range(len(listbox_list)):
-#             file.write(listbox_list[i] + "\n")
-            
-# # #this function is called when the window is closed. It writes the listbox_list to the file
-# # def on_closing():
-# #     with open("tasks.txt", "w") as file:
-# #         for i in range(len(listbox_list)):
-#             file.write(listbox_list[i] + "\n")
-#     main.destroy()
-
+    
 
 
 
 
 #-------------Buttons------------------#
-add_button = tk.Button(button_frame, text = "Add", command = add_button_function, width= button_width, font=(10))
+add_button = tk.Button(add_button_frame, text = "Add", command = add_button_function, width= button_width, font=(10))
 add_button.grid(row = 0,column = 0)
+
+tk.Button(done_button_frame,text =  "Done", command= done_function, width= button_width, font=(10)).grid(row = 0, column= 1)
+
+tk.Button(undo_frame, text = "Undo", command = undo, width= button_width - 10).grid(row = 0, column = 0)
+tk.Button(undo_frame, text = "Delete", command = delete_item, width= button_width - 10).grid(row = 0, column = 1)
 
 
 #-----------Keyboard Navigation--------------#
@@ -193,6 +190,4 @@ my_entry.bind('<FocusIn>', on_focus_in)
 my_entry.bind('<FocusOut>', on_focus_out)
 
 
-tk.Button(delete_frame,text =  "Done", command= delete_item, width= button_width, font=(10)).grid(row = 0, column= 1)
-tk.Button(completed_frame, text = "  Undo  ", command = undo).grid(row = 0, column = 0)
 tk.mainloop()
